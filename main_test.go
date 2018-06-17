ea8d614137fe7fef5c668d367aa3aa82cb1399c8
pmartin <contact@elol.fr>
2018-06-17 17:07:19 +0200

    and testing it

--- main_test.go
+++ main_test.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"errors"
 	"testing"
 
+	authorizationv1 "k8s.io/api/authorization/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/version"
 	discoveryfake "k8s.io/client-go/discovery/fake"
 	"k8s.io/client-go/kubernetes/fake"
+	k8stesting "k8s.io/client-go/testing"
 )
 
 func newTestSimpleK8s() *k8s {
 	client := k8s{}
 	client.clientset = fake.NewSimpleClientset()
+	return &client
+}
+
+func newTestK8s() *k8s {
+	client := k8s{
+		clientset: &fake.Clientset{},
+	}
 	return &client
 }
 
@@ -76,5 +87,47 @@ func TestIsVersionErrorMinor(t *testing.T) {
 	expected := "Minor version does not match"
 	if err.Error() != expected {
 		t.Fatal("Raised error should be: " + expected)
+	}
+}
+
+func TestCanICreateDeploymentsFalse(t *testing.T) {
+	k8s := newTestSimpleK8s()
+	c, err := k8s.canICreateDeployments()
+	if err != nil {
+		t.Fatal("canICreateDeployments should not raise an error")
+	}
+	if c != false {
+		t.Fatal("canICreateDeployments should return false")
+	}
+}
+
+func TestCanICreateDeplaoymentsTrue(t *testing.T) {
+	k8s := newTestK8s()
+	k8s.clientset.(*fake.Clientset).Fake.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+		mysar := &authorizationv1.SelfSubjectAccessReview{
+			Status: authorizationv1.SubjectAccessReviewStatus{
+				Allowed: true,
+				Reason:  "I want to test it",
+			},
+		}
+		return true, mysar, nil
+	})
+	c, err := k8s.canICreateDeployments()
+	if err != nil {
+		t.Fatal("canICreateDeployments should not raise an error")
+	}
+	if c != true {
+		t.Fatal("canICreateDeployments should return true")
+	}
+}
+
+func TestCanICreateDeplaoymentsError(t *testing.T) {
+	k8s := newTestK8s()
+	k8s.clientset.(*fake.Clientset).Fake.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+		return true, &authorizationv1.SelfSubjectAccessReview{}, errors.New("Error creating ssar")
+	})
+	_, err := k8s.canICreateDeployments()
+	if err == nil {
+		t.Fatal("canICreateDeployments should raise an error")
 	}
 }
